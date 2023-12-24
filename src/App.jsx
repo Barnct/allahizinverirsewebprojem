36da9397ba94cd697cc3b832a772da130917f16b
Barnct <albayrakbaranct@gmail.com>
2023-12-24 15:37:11 +0300

    react üzeirne oynamalar denemeler

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,18 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import Button from "./components/Button"
+import Text from "./components/text"
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  var name = "abi"
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        {name}
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Text number = "1" name="react"></Text>
+      <Text number = "2" name="reactt"></Text>
+      <Text number = "3" name="reacttt"></Text>
+      <Button name="arttir"></Button>
+      <Button name="azalt"></Button>
     </>
   )
 }
---
+++ src/components/Button.jsx
@@ -0,0 +1,11 @@
+
+
+const Button = ({name}) => {
+  return (
+    <button>
+        {name}
+    </button>
+  )
+}
+
+export default Button
